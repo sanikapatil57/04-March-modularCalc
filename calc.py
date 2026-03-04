a603902e9018c1568808f9aae803e1c98b46fa76
Sanika Patil <sanikapatil5723@gmail.com>
2026-03-04 17:47:29 +0530

    Modularity added

--- calc.py
+++ calc.py
@@ -1,37 +1,3 @@
-class Calculator:
-
-    def __init__(self, first_number, operation, second_number):
-        self.first_number = first_number
-        self.operation = operation
-        self.second_number = second_number
-
-    def add(self):
-        return self.first_number + self.second_number
-
-    def subtract(self):
-        return self.first_number - self.second_number
-
-    def multiply(self):
-        return self.first_number * self.second_number
-
-    def divide(self):
-        if self.second_number == 0:
-            return "Error: Division by zero is not allowed."
-        return self.first_number / self.second_number
-
-
-# Advanced calculator using inheritance
-class AdvancedCalculator(Calculator):
-
-    def power(self):
-        return self.first_number ** self.second_number
-
-    def square_root(self):
-        if self.first_number < 0:
-            return "Error: Cannot calculate square root of a negative number."
-        return self.first_number ** 0.5
-
-
 def main():
 
     while True:
